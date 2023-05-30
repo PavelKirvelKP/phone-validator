049b99a796628221d67b2f3a09b54b6a3dacea8e
Pavel Kirvel <kirvel@textkernel.nl>
2023-05-30 21:34:11 +0300

    Implement demo phone validator service

--- src/main/java/com/kirvel/phonevalidator/exception/CountryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.kirvel.phonevalidator.exception;
-
-public class CountryNotFoundException extends RuntimeException {
-	public CountryNotFoundException(String message) {
-		super(message);
-	}
-}
